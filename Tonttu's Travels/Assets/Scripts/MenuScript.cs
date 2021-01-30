d28b3c0c15f5a5ffdb931ef6d4cf6ab120a7d912
Georgi Tsaklev <georgi.tsaklev@gmail.com>
2021-01-30 10:39:43 +0000

    Added main menu

---
+++ Tonttu's Travels/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void Quit() {
+        Application.Quit();
+    }
+
+    public void Play() {
+        SceneManager.LoadScene(1);
+    }
+}
