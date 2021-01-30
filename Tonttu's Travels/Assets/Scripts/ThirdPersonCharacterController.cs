c3f31b142325bbed4594a9f9cd1e137563fa1c3a
Alex Asenov <tsukiall@gmail.com>
2021-01-30 21:35:50 +0200

    Script character move, run, and jump animation

--- Tonttu's Travels/Assets/Scripts/ThirdPersonCharacterController.cs
+++ Tonttu's Travels/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -4,6 +4,7 @@ public class ThirdPersonCharacterController : MonoBehaviour
 {
   public GameObject cam;
 
+  private Animator animator;
   private Rigidbody rb;
 
   #region IO
@@ -159,6 +160,7 @@ public class ThirdPersonCharacterController : MonoBehaviour
       if (isSprinting)
       {
         speed = GetSpeed(sprintSpeed);
+        animator.SetFloat("Locomotion", 1.0f);
       }
       else if (isInStealth)
       {
@@ -171,10 +173,15 @@ public class ThirdPersonCharacterController : MonoBehaviour
       else
       {
         speed = GetSpeed(movementSpeed);
+        animator.SetFloat("Locomotion", 0.5f);
       }
 
       Vector3 moveDir = Quaternion.Euler(0f, inputAngle, 0f) * Vector3.forward;
       rb.MovePosition(transform.position + moveDir.normalized * speed * Time.fixedDeltaTime);
+    }
+    else
+    {
+      animator.SetFloat("Locomotion", 0.0f);
     }
   }
 
@@ -182,6 +189,7 @@ public class ThirdPersonCharacterController : MonoBehaviour
   {
     if (isJumping)
     {
+      animator.SetBool("Jump", true);
       isJumping = false;
       float speed = onGround ? jumpSpeed : airJumpSpeed;
       rb.AddForce(Vector3.up * speed, ForceMode.Impulse);
@@ -209,9 +217,26 @@ public class ThirdPersonCharacterController : MonoBehaviour
     }
   }
 
+  void OnCollisionStay(Collision collision)
+  {
+    if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+    {
+      animator.SetBool("isFalling", false);
+    }
+  }
+
+  void OnCollisionExit(Collision collision)
+  {
+    if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+    {
+      animator.SetBool("isFalling", true);
+    }
+  }
+
   void Start()
   {
     rb = GetComponentInChildren<Rigidbody>();
+    animator = GetComponentInChildren<Animator>();
     Cursor.lockState = CursorLockMode.Locked;
     Cursor.visible = false;
   }
