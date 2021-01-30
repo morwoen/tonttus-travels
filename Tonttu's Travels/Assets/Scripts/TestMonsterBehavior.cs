150fce8a9b9d19f4cb6cac3117250ec7af1bbbc4
stargazer23 <gvenios93@gmail.com>
2021-01-30 13:29:42 +0200

    patrol

--- Tonttu's Travels/Assets/Scripts/TestMonsterBehavior.cs
+++ Tonttu's Travels/Assets/Scripts/TestMonsterBehavior.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
-using System.Collections;
 
-
-public class PatrolScript : MonoBehaviour
+public class TestMonsterBehavior : MonoBehaviour
 {
 
+    public float distanceToHunt = 10f;
+    Transform target;
     public Transform[] points;
     private int destPoint = 0;
     public NavMeshAgent agent;
@@ -14,15 +16,16 @@ public class PatrolScript : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        target = PlayerManager.instance.transform;
 
 
-        
+        agent.autoBraking = false;
 
         GotoNextPoint();
     }
 
 
-    public void GotoNextPoint()
+    void GotoNextPoint()
     {
 
         if (points.Length == 0)
@@ -37,8 +40,14 @@ public class PatrolScript : MonoBehaviour
 
     void Update()
     {
+        float distance = Vector3.Distance(target.position, transform.position);
 
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
             GotoNextPoint();
+
+        if (distance <= distanceToHunt)
+        {
+            agent.SetDestination(target.position);
+        }
     }
 }
