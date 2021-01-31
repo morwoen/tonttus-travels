54c2cb450c83dfd5d44914280b0578066752616d
Alex Asenov <tsukiall@gmail.com>
2021-01-31 15:31:12 +0200

    Rework enemy movement and detection

--- Tonttu's Travels/Assets/Scripts/Enemies/MonsterBehavior.cs
+++ Tonttu's Travels/Assets/Scripts/Enemies/MonsterBehavior.cs
@@ -9,50 +9,106 @@ public class MonsterBehavior : MonoBehaviour
   public GameObject target;
   public float distanceToKill = 2f;
   public float distanceToHunt = 10f;
+  public float patrolSpeed = 5.0f;
+  public float chaseSpeed = 10.0f;
+  public float checkpointDistance = 0.1f;
+  public float turnTime = 10f;
 
+  private Rigidbody rb;
   private Animator animator;
   private NavMeshAgent agent;
   private PatrolScript patrol;
-  private bool isChasing = false;
+  private float turnSmoothVelocity;
 
   void Start()
   {
     agent = GetComponent<NavMeshAgent>();
     animator = GetComponentInChildren<Animator>();
     patrol = GetComponent<PatrolScript>();
+    rb = GetComponent<Rigidbody>();
   }
 
-  void Update()
+  void FixedUpdate()
   {
-    if (target) {
-        float distance = Vector3.Distance(target.transform.position, transform.position);
+    if (target)
+    {
+      float distance = Vector3.Distance(target.transform.position, transform.position);
 
-        if (distance <= distanceToHunt)
-        {
-          animator.SetBool("Alert", true);
-          agent.SetDestination(target.transform.position);
-          isChasing = true;
-        }
+      if (distance < checkpointDistance)
+      {
+        SetTarget(patrol.GetNextPoint());
+      }
 
-        if (isChasing && distance > distanceToHunt)
+      Vector3 moveDirection = target.transform.position - transform.position;
+      float speed = target.CompareTag("Player") ? chaseSpeed : patrolSpeed;
+      transform.position = transform.position + moveDirection.normalized * speed * Time.fixedDeltaTime;
+
+      Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+
+      transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnTime * Time.fixedDeltaTime);
+
+      if (target.CompareTag("Player"))
+      {
+        RaycastHit hit;
+
+        bool isTargetFound = Physics.Raycast(transform.position, target.transform.position - transform.position, out hit, distanceToHunt);
+
+        if (!isTargetFound || !hit.collider.gameObject.CompareTag("Player"))
         {
-          animator.SetBool("Alert", false);
-          patrol.GotoNextPoint();
-          SetTarget(null);
+          SetTarget(patrol.GetNextPoint());
+          return;
         }
 
         if (distance <= distanceToKill)
         {
-            target.GetComponent<PlayerCheckpoint>().LoadCheckpoint();
+          target.GetComponent<PlayerCheckpoint>().LoadCheckpoint();
+          SetTarget(patrol.GetNextPoint());
+          animator.SetBool("Alert", false);
         }
+      }
+    }
+    else
+    {
+      SetTarget(patrol.GetNextPoint());
     }
   }
 
-  public void SetTarget(GameObject player) {
-     target = player;
+  public void SetTarget(GameObject player)
+  {
+    target = player;
   }
 
-  public bool HasTarget() {
+  public bool HasTarget()
+  {
     return !!target;
+  }
+
+  void OnTriggerStay(Collider collider)
+  {
+    if (collider.CompareTag("Player"))
+    {
+      RaycastHit hit;
+
+      bool isTargetFound = Physics.Raycast(transform.position, collider.transform.position - transform.position, out hit, distanceToHunt);
+      Debug.DrawRay(transform.position, collider.transform.position - transform.position, Color.blue);
+
+      if (!isTargetFound || (isTargetFound && !hit.collider.gameObject.CompareTag("Player")))
+      {
+        SetTarget(patrol.GetNextPoint());
+        return;
+      }
+
+      SetTarget(collider.gameObject);
+      animator.SetBool("Alert", true);
+    }
+  }
+
+  void OnTriggerExit(Collider collider)
+  {
+    if (collider.CompareTag("Player"))
+    {
+      SetTarget(patrol.GetNextPoint());
+      animator.SetBool("Alert", false);
+    }
   }
 }
