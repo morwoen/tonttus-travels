0f04438e5f2248deaaeb7b4b61256f4f738eec89
MattBaileyDev <mb214650@falmouth.ac.uk>
2021-01-31 13:46:20 +0000

    Tutorial Finished

---
+++ Tonttu's Travels/Assets/Scripts/TutorialTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+
+    
+    public GameObject Object;
+    public GameObject UIMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            UIMessage.SetActive(true);
+        }
+        
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            UIMessage.SetActive(false);
+            Invoke("DestroyTrigger", 5);
+        }
+        
+    }
+
+    void DestroyTrigger()
+    {
+        UIMessage.SetActive(false);
+        Destroy(Object);
+    }
+
+  
+  
+}
